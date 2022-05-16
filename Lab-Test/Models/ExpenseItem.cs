8ffe362918e8cd91955b060387db63fda4908ec4
chittoranjan <chittoranjan.sarker@gmail.com>
2022-05-16 23:53:08 +0600

    initial

---
+++ Lab-Test/Models/ExpenseItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_Test.Models
+{
+    public class ExpenseItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Lab-Test/Models/LabTestContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab_Test.Models
+{
+    public class LabTestContext:DbContext
+    {
+        private readonly DbContextOptions _context;
+        public LabTestContext(DbContextOptions context) : base(context)
+        {
+            _context = context;
+        }
+        public DbSet<ExpenseItem> ExpenseItems { get; set; }
+    }
+}
