d40735408fb7d7e9703defe96777131c276a5bdd
chittoranjan <chittoranjan.sarker@gmail.com>
2023-03-25 00:42:26 +0600

    Lab-Test.Test project added

---
+++ Lab-Test.Test/ExpenseItemTests.cs
@@ -0,0 +1,27 @@
+using Lab_Test.Controllers.ExpenseControllers;
+using Model.DtoModels.ExpenseDtoModels;
+
+namespace Lab_Test.Test
+{
+    [TestClass]
+    public class ExpenseItemTests
+    {
+        [TestMethod]
+        public void Add_WithValidModel_ReturnObject()
+        {
+            //// Arrange
+            //int id = 0;
+            //string name = "ABC";
+            //string description = "Description";
+            //int unitPrice = 25;
+            //ExpenseItemDto expected = new ExpenseItemDto(){Id=id,Name=name,Description=description,UnitPrice=unitPrice };
+
+            //// Act
+            //ExpenseItemDto actual= ;
+
+            //// Assert
+          
+            //Assert.AreEqual(expected, actual);
+        }
+    }
+}
